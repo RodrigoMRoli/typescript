493b65ba86303bb8f7de5a86c7554d9fac21473f
Rodrigo Monteiro Roli <rodrigo.monteiro.roli@gmail.com>
2023-08-06 17:49:47 -0300

    fim capitulo 2

--- app/app.ts
+++ app/app.ts
@@ -2,11 +2,15 @@ import { NegociacaoController } from './controllers/negociacao-controller.js';
 // import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController()
-const form = document.querySelector('.form')
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    controller.adiciona()
-});
+const form = document.querySelector('.form') as HTMLInputElement
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        controller.adiciona()
+    });    
+} else {
+    throw new Error('Não foi possível iniciar a aplicação. Verifique se o form existe')
+}
 
 // const negociacoesView = new NegociacoesView
 // const template = negociacoesView.template()
