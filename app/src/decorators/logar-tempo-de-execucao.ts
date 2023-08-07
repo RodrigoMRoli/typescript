48a0f6860aea5a57f4c2421b051034b827a47695
Rodrigo Monteiro Roli <rodrigo.monteiro.roli@gmail.com>
2023-08-07 17:18:14 -0300

    fim módulo 2

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function (
         target: any, // se você coloca num método estático, target é a função construtora, se você coloca num método normal é o prototype da classe
         propertyKey: string, // nome do método que foi decorado
@@ -6,11 +6,17 @@ export function logarTempoDeExecucao() {
     ) {
         const metodoOriginal = descriptor.value
         descriptor.value = function (...args: Array<any>) {
+            let divisor = 1
+            let unidade = 'milisegundos'
+            if (emSegundos) {
+                divisor = 1000
+                unidade = 'segundos'
+            }
             const t1 = performance.now()
             const retorno = metodoOriginal.apply(this, args)
             const t2 = performance.now()
-            const tempoDeExecucao = (t2 - t1) / 1000
-            console.log(`${propertyKey}, tempo de execução: ${(tempoDeExecucao)} segundos`)
+            const tempoDeExecucao = (t2 - t1) / divisor
+            console.log(`${propertyKey}, tempo de execução: ${(tempoDeExecucao)} ${unidade}`)
             retorno
         }
 
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js"
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js"
 
 export abstract class View<T> {
@@ -5,22 +6,19 @@ export abstract class View<T> {
     protected elemento: HTMLElement
     private escapar = false
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor)
         if (elemento) {
             this.elemento = elemento as HTMLInputElement
         } else {
             throw new Error(`Seletor ${seletor} não existe no DOM. Verifique com o desenvolvedor.`)
         }
-        if (escapar) this.escapar = escapar
     }
     
-    @logarTempoDeExecucao()
+    @logarTempoDeExecucao(true)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model)
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '')
-        }
         this.elemento.innerHTML = template
     }
 
